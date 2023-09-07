bc471fab3278f75de48028e77797323ad38cb538
viettv1 <viettv1@smartosc.com>
2023-09-07 10:41:34 +0700

    init

---
+++ app/oke/page.tsx
@@ -0,0 +1,7 @@
+import HomePage from "../../components/HomePage";
+
+export const runtime = "edge";
+
+export default function Page() {
+  return <HomePage />;
+}
---
+++ components/HomePage.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const HomePage = () => {
+  const router = useRouter();
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
