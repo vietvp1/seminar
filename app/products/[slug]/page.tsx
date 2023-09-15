b6d100c9335ffc0bb33fe0ef6e13499a90509e3e
viettv1 <viettv1@smartosc.com>
2023-09-15 19:05:21 +0700

    update

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import ProductPage from "@/components/ProductPage";
 import { createClient } from "@/prismicio";
 
 type Props = {
@@ -8,7 +9,7 @@ export const revalidate = 60;
 
 export async function generateStaticParams() {
   const products = [];
-  for (let i = 0; i < 500; i++) {
+  for (let i = 0; i < 1000; i++) {
     products.push({ slug: `slug-${i}` });
   }
   return products;
@@ -17,5 +18,10 @@ export async function generateStaticParams() {
 export default async function Product({ params: { slug } }: Props) {
   const client = createClient();
   const a = await client.getSingle("homepage");
-  return <div>{slug}</div>;
+  return (
+    <div>
+      {slug}
+      <ProductPage a={a} />
+    </div>
+  );
 }
---
+++ components/ProductPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductPage = (props: any) => {
+  return (
+    <div>ProductPage {props.a.data.title}</div>
+  )
+}
+
+export default ProductPage
