a243cea849703f0ff611808dc1b0cea293e83e05
viettv1 <viettv1@smartosc.com>
2023-09-15 14:24:38 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import HomePage from "@/components/HomePage";
 import { createClient } from "@/prismicio";
 
+export const revalidate = 60;
+
 export default async function Page() {
   const client = createClient()
   const a = await client.getSingle('homepage')
