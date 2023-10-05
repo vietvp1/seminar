8614d163d7a31f7752e90fe08cf9f67ccc7d53b8
viettv1 <viettv1@smartosc.com>
2023-10-05 16:06:35 +0700

    test data

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,13 +1,29 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [data, setData] = useState<any[]>([]);
   useEffect(() => {
     console.log("HomePage client side");
+    const fetchData = async function () {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+      const json = await res.json();
+      setData(json);
+    };
+    fetchData();
   }, []);
 
-  return <div>HomePage Okeeeeeeee</div>;
+  return (
+    <div>
+      HomePage Okeeeeeeee
+      <div>
+        {data.map((item, i) => (
+          <div style={{border: 'solid 1px lightgray'}} key={i}>{item.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
